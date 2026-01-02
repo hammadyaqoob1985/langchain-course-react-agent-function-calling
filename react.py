6899a6ee0e948c466cb49e202de6ee6443b185de
hammadyaqoob1985 <hammad.yaqoob1985@yahoo.co.uk>
2026-01-02 03:36:06 +0000

    function calling reasoning

---
+++ react.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+@tool
+def triple(num:float) -> float:
+    """
+    Triple a number.
+    Args:
+        num: The number to triple.
+    Returns:
+        The triple of the number.
+    """
+    return num * 3
+
+
+tools = [TavilySearch(max_results=1), triple]
+
+
+
